6960b2721ea12cac52f04dee0a78b750fdc81100
Jie Ren <renjie@megvii.com>
2021-10-21 23:05:07 +0800

    init

---
+++ Timer.cpp
@@ -0,0 +1,142 @@
+//
+// Created by Jie Ren (jieren9806@gmail.com) on 2021/10/21.
+//
+
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include "Timer.h"
+
+namespace Timer {
+    namespace {
+        template<typename Iterator>
+        inline void ExistChecker(const Iterator &find, const Iterator &end, const std::string &name) {
+            if (find == end)
+                throw std::runtime_error("name {" + name + "} not exist");
+        }
+
+        template<typename Duration_t, typename Node_t>
+        void PrintOneNode(const Duration_t &duration, const Node_t root, const std::string &name, int level) {
+            if (level >= 0) {
+                auto find = duration.find(root.get());
+                ExistChecker(find, duration.end(), name);
+                if (level > 0)
+                    std::cout
+                            << std::setw(5 * (level - 1) + 1)
+                            << std::setfill(' ')
+                            << '|'
+                            << std::setw(5)
+                            << std::setfill('-');
+                std::cout
+                        << name
+                        << ": "
+                        << std::chrono::duration_cast<TIMER_REPORT_TIME_TYPE>(find->second).count()
+                        << TIMER_REPORT_TIME_NAME
+                        << std::endl;
+            }
+            for (const auto &node: root->descendants_) {
+                PrintOneNode(duration, node.second, node.first, level + 1);
+            }
+        }
+
+        template<typename Node_t>
+        void PushDescendants(const Node_t root, std::vector<std::string> &names) {
+            for (const auto &node: root->descendants_) {
+                names.push_back(node.first);
+                PushDescendants(node.second, names);
+            }
+        }
+
+        template<typename RelationTree_t>
+       inline auto getNodePtr(const std::string &name, const RelationTree_t &rt) {
+            auto find = rt.plain_nodes_.find(name);
+            ExistChecker(find, rt.plain_nodes_.end(), name);
+            return find->second.get();
+        }
+
+        struct RelationNode {
+            std::unordered_map<std::string, std::shared_ptr<RelationNode>> descendants_{};
+            std::weak_ptr<RelationNode> parent;
+            explicit RelationNode(const std::shared_ptr<RelationNode>& father_ptr) : parent(father_ptr) {};
+        };
+
+        struct RelationTree {
+            std::shared_ptr<RelationNode> root_{new RelationNode{nullptr}};
+            std::map<std::string, std::shared_ptr<RelationNode>> plain_nodes_{};
+        } rt{};
+
+        std::map<void *, TIMER_RECORD_TIME_TYPE> duration_{};
+        std::map<void *, std::chrono::time_point<std::chrono::system_clock>> start_time_{};
+    }
+
+    void Timer::StartRecording(const std::string &name) {
+        if (rt.plain_nodes_.find(name) == rt.plain_nodes_.end()) {
+            std::shared_ptr<RelationNode> node_ptr{new RelationNode{rt.root_}};
+            rt.root_->descendants_.emplace(name, node_ptr);
+            rt.plain_nodes_.emplace(name, std::move(node_ptr));
+        } else if (rt.root_->descendants_.find(name) == rt.root_->descendants_.end()) {
+            throw std::runtime_error("name {" + name + "} duplicated");
+        }
+        start_time_[getNodePtr(name, rt)] = std::chrono::system_clock::now();
+    }
+
+    void Timer::StartRecording(const std::string &name, const std::string &father_name) {
+        auto father_find = rt.plain_nodes_.find(father_name);
+        auto find = rt.plain_nodes_.find(name);
+        ExistChecker(father_find, rt.plain_nodes_.end(), father_name);
+        if (rt.plain_nodes_.find(name) == rt.plain_nodes_.end()) {
+            std::shared_ptr<RelationNode> node_ptr{new RelationNode{father_find->second}};
+            father_find->second->descendants_.emplace(name, node_ptr);
+            rt.plain_nodes_.emplace(name, std::move(node_ptr));
+        } else if (father_find->second->descendants_.find(name) == father_find->second->descendants_.end()) {
+            throw std::runtime_error("name {" + name + "} duplicated");
+        }
+        start_time_[getNodePtr(name, rt)] = std::chrono::system_clock::now();
+    }
+
+    void Timer::StopRecording(const std::string &name) {
+        RelationNode *node_ptr = getNodePtr(name, rt);
+        auto end_time_point = std::chrono::system_clock::now();
+        ExistChecker(start_time_.find(node_ptr), start_time_.end(), name);
+        const auto duration_find = duration_.find(node_ptr);
+        if (duration_find == duration_.end())
+            duration_.emplace(node_ptr, end_time_point - start_time_.find(node_ptr)->second);
+        else
+            duration_find->second += end_time_point - start_time_.find(node_ptr)->second;
+    }
+
+    void Timer::Erase(const std::string &name) {
+        auto find = rt.plain_nodes_.find(name);
+        ExistChecker(find, rt.plain_nodes_.end(), name);
+        std::vector<std::string> nodes_to_be_delete{name};
+        PushDescendants(find->second, nodes_to_be_delete);
+        find->second->parent.lock()->descendants_.erase(name);
+        for (const auto &descendants_name: nodes_to_be_delete)
+            rt.plain_nodes_.erase(descendants_name);
+    }
+
+    void Timer::ResetAll() {
+        for (auto &duration: duration_)
+            duration.second = TIMER_RECORD_TIME_TYPE::zero();
+    }
+
+    void Timer::Reset(const std::string &name) {
+        auto find = rt.plain_nodes_.find(name);
+        ExistChecker(find, rt.plain_nodes_.end(), name);
+        auto ptr_find = duration_.find(find->second.get());
+        ExistChecker(ptr_find, duration_.end(), name);
+        ptr_find->second = TIMER_RECORD_TIME_TYPE::zero();
+    }
+
+    void Timer::ReportAll() {
+        std::cout << "Report all the results in the recorder:" << std::endl;
+        PrintOneNode(duration_, rt.root_, "root", -1);
+    }
+
+    void Timer::Report(const std::string &name) {
+        auto find = rt.plain_nodes_.find(name);
+        ExistChecker(find, rt.plain_nodes_.end(), name);
+        std::cout << "Report {" + name + "} in the recorder:" << std::endl;
+        PrintOneNode(duration_, find->second, name, 0);
+    }
+}
---
+++ Timer.h
@@ -0,0 +1,38 @@
+//
+// Created by Jie Ren (jieren9806@gmail.com) on 2021/10/21.
+//
+
+#ifndef TIMER_TIMER_H
+#define TIMER_TIMER_H
+
+#define TIMER_RECORD_TIME_TYPE std::chrono::nanoseconds
+#define TIMER_REPORT_TIME_TYPE std::chrono::milliseconds
+#define TIMER_REPORT_TIME_NAME "ms"
+
+#include <chrono>
+#include <map>
+#include <unordered_map>
+#include <memory>
+#include <set>
+
+namespace Timer {
+    struct Timer {
+        static void StartRecording(const std::string &name);
+
+        static void StartRecording(const std::string &name, const std::string &father_name);
+
+        static void StopRecording(const std::string &name);
+
+        static void Erase(const std::string &name);
+
+        static void ResetAll();
+
+        static void Reset(const std::string &name);
+
+        static void ReportAll();
+
+        static void Report(const std::string &name);
+    };
+}
+
+#endif //TIMER_TIMER_H
